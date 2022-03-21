b6d99d0679d6f825106294b92c877e78215ed17a
Olivier <olivier.elophe@gmail.com>
2022-03-22 00:44:27 +0100

    base du programme de chiffrement

--- main.py
+++ main.py
@@ -1,37 +1,57 @@
-
-# https://realpython.com/python-gui-tkinter/
-
 import tkinter as tk
 
-matrice = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-]
 
-def handle_click(event):
-    print("The button was clicked!", event, matrice)
-
-window = tk.Tk()
-
-for i in range(3):
-    window.columnconfigure(i, weight=1, minsize=75)
-    window.rowconfigure(i, weight=1, minsize=50)
-
-    for j in range(0, 3):
-        frame = tk.Frame(
-            master=window,
-            relief=tk.RAISED,
-            borderwidth=0
-        )
-        frame.grid(row=i, column=j, padx=5, pady=5)
-        button = tk.Button(master=frame,
-                           bg="black",
-                           fg="white",
-                           text=f"Row {i}\nColumn {j}")
-        button.bind("<Button-1>", handle_click)
-        button.pack(padx=5, pady=5)
-events_list = []
+# On crée la fenêtre principale avec un titre et un fond
+fenetre = tk.Tk()
+fenetre.title("CryptoSturm")
+fenetre['bg'] = '#FA8072'
 
 
-window.mainloop()
+def negatif(im):
+    for i in range(H): # On parcours les lignes de pixels de l'image
+        for j in range(L): # On parcours les pixels de la ligne i
+            (r, v, b) = originale.getpixel((j, i))
+            nouvelle_image.putpixel((j, i), (255 - r, 255 - v, 255 - b))
+    nouvelle_image.save("image/negatif.jpg", "JPEG")
+    variable.set("image/negatif.jpg")
+    im.tkimage = ImageTk.PhotoImage(nouvelle_image)
+    canvas2.itemconfig(1, image=im.tkimage)
+
+
+def claire(n, im):
+    for i in range(H):  # On parcours les lignes de pixels de l'image
+        for j in range(L):  # On parcours les pixels de la ligne i
+            (r, v, b) = originale.getpixel((j, i))
+            nouvelle_image.putpixel((j, i), (r + n, v + n, b + n))
+    nouvelle_image.save("image/plusclair.jpg", "JPEG")
+    variable.set("image/plusclair.jpg")
+    im.tkimage = ImageTk.PhotoImage(nouvelle_image)
+    canvas2.itemconfig(1, image=im.tkimage)
+
+
+# Nous allons créer deux frames
+message_clair = tk.LabelFrame (fenetre,text="Message en clair", bg="green", width=500, height=500)
+message_clair.grid(row=0, column=0, sticky='W')
+message_chiffre = tk.LabelFrame (fenetre,text="Message chiffré", bg="red", width=500, height=500)
+message_chiffre.grid(row=0, column=1, sticky='W')
+# à l'interieur de chacune des frames
+bouton_negatif = tk.Button(message_clair, text="Hash", command=lambda: negatif(mon_image))
+bouton_negatif.grid(row=2, column=0, sticky='N', pady=2)
+bouton_clair = tk.Button(message_chiffre, text="Check hash", command=lambda: claire(200, mon_image))
+bouton_clair.grid(row=2, column=1, sticky='N', pady=2)
+# Les inputs
+variable = tk.StringVar()
+variable.set("")
+e1 = tk.Entry(message_clair, textvariable=variable, width=50, fg='blue')
+e2 = tk.Entry(message_chiffre, bg='#999999', width=50)
+e1.grid(row=0, column=0, columnspan=6, sticky='W', ipadx=10, ipady=50)
+e2.grid(row=0, column=0, columnspan=6, sticky='W', ipadx=10, ipady=50)
+# Le bouton de chiffrement
+bouton = tk.Button(fenetre, text="process", fg="red", bg="white", relief="raised",
+                overrelief="sunken")
+bouton.grid(row=1, column=0, columnspan=2, sticky='N'+'W'+'E', pady=2)
+# Le bouton qui permet de quitter l'application.
+bouton = tk.Button(fenetre, text="Quit", fg="red", bg="white", relief="raised",
+                overrelief="sunken", command=fenetre.destroy)
+bouton.grid(row=2, column=0, columnspan=2, sticky='N'+'W'+'E', pady=2)
+fenetre.mainloop()
