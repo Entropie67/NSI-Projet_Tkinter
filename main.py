d8720015826eda2f6401a3c5167693b42aaaa6ea
Entropie67 <olivier.elophe@gmail.com>
2022-03-22 09:18:47 +0100

    CryptoSturm fonctionnel

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ fenetre['bg'] = '#FA8072'
 
 def chiffrement():
     m = message.get()
-    m = "".join([str(ord(l)) for l in m.upper()])
+    m = "".join([chr(ord(l) + 3) for l in m.upper()])
     message_c.set(m)
 
 
 def dechiffrer():
     m = message_c.get()
-    m = "-".join([chr(int(m[i:i+2])) for i in range(len(m))])
+    m = "".join([chr(ord(l) - 3) for l in m])
     message.set(m)
+
+def clear():
+    message_c.set("")
+    message.set("")
+
 
 # Nous allons créer deux frames
 message_clair = tk.LabelFrame (fenetre,text="Message en clair", bg="green", width=500, height=500)
@@ -32,14 +37,14 @@ bouton_clair.grid(row=2, column=1, sticky='N', pady=2)
 message = tk.StringVar()
 message.set("Taper le message que vous souhaitez chiffrer.")
 message_c = tk.StringVar()
-message_c.set("lkdjfqlghqsiefjskldsj:fkdngv,.")
+message_c.set("Je suis bidon")
 e1 = tk.Entry(message_clair, textvariable=message, width=50, fg='blue')
 e2 = tk.Entry(message_chiffre, textvariable=message_c, bg='#999999', width=50)
 e1.grid(row=0, column=0, columnspan=6, sticky='W', ipadx=10, ipady=50)
 e2.grid(row=0, column=0, columnspan=6, sticky='W', ipadx=10, ipady=50)
 # Le bouton de chiffrement
-bouton = tk.Button(fenetre, text="process", fg="red", bg="white", relief="raised",
-                overrelief="sunken")
+bouton = tk.Button(fenetre, text="Clear", fg="red", bg="white", relief="raised",
+                overrelief="sunken", command=clear)
 bouton.grid(row=1, column=0, columnspan=2, sticky='N'+'W'+'E', pady=2)
 # Le bouton qui permet de quitter l'application.
 bouton = tk.Button(fenetre, text="Quit", fg="red", bg="white", relief="raised",
